2dda6aef82a5b924981cce57510ba162029a4a98
BuildTools <mods.itsmeow@gmail.com>
2017-09-17 00:46:26 -0400

    Complete GUI & Music structure, no player implemented.

--- java/its_meow/mcmusicplayer/MusicManager.java
+++ java/its_meow/mcmusicplayer/MusicManager.java
@@ -3,36 +3,45 @@ package its_meow.mcmusicplayer;
 import java.io.File;
 import java.io.IOException;
 
+import javax.sound.sampled.AudioFileFormat.Type;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioFormat.Encoding;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.DataLine.Info;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import com.connormahaffey.JavaMusicExample.Music;
+
 import its_meow.mcmusicplayer.proxy.CommonProxy;
+import javazoom.spi.mpeg.sampled.file.MpegAudioFileReader;
 import net.minecraft.client.Minecraft;
 
 public class MusicManager {
 
 	File songFolder;
 	int trackNum = -1;
-	AudioInputStream ais;
-	Clip clip;
+	Music music;
 	
 	public void init() throws LineUnavailableException {
 		songFolder = new File(CommonProxy.configDirectory, "/mcmusicplayer/songs/");
-		clip = AudioSystem.getClip();
 		if(!songFolder.exists()) {
 			songFolder.mkdirs();
 		}
+		music = new Music();
 	}
 
 	public File[] songsInFolder() {
 		int mp3count = 0;
 		File[] files = songFolder.listFiles();
-		File[] mp3s = {null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null};
+		File[] mp3s = new File[300];
 		if(files.length != 0) {
-			for(int i = 0; i <= files.length; i++) {
+			System.out.println("Files found in songs folder: " + files.length);
+			for(int i = 0; i < files.length; i++) {
 				File fileIn = files[i];
 				if(fileIn.getAbsolutePath().endsWith(".mp3")) {
 					mp3count++;
@@ -45,13 +54,13 @@ public class MusicManager {
 	
 	public void nextSong() {
 		if(trackNum == -1) {
-			clip.stop(); // Close old song
+			music.stop(); // Close old song
 			playSong();
 		} else {
 			File[] songs = songsInFolder();
 			if(trackNum != songs.length) {
 				trackNum++;
-				clip.stop(); // Close old song
+				music.stop(); // Close old song
 				playSong();
 			}
 		}
@@ -63,12 +72,12 @@ public class MusicManager {
 		} else {
 			if(trackNum != 0) {
 				trackNum--;
-				clip.stop(); // Close old song
+				music.stop(); // Close old song
 				playSong();
 			} else {
 				File[] songs = songsInFolder();
 				trackNum = songs.length;
-				clip.stop(); // Close old song
+				music.stop(); // Close old song
 				playSong();
 			}
 		}
@@ -78,7 +87,7 @@ public class MusicManager {
 		if(trackNum == -1) {
 			playSong();
 		} else {
-			clip.stop();
+			music.stop();
 		}
 	}
 	
@@ -86,21 +95,25 @@ public class MusicManager {
 		File[] songs = songsInFolder();
 		if(trackNum == -1) {
 			if(songs.length > 0) {
-				trackNum = 1;
+				trackNum = 0;
 			} else {
 				return;
 			}
 		}
-		
 		File song = songs[trackNum];
-		try {
-			ais = AudioSystem.getAudioInputStream(song);
-			clip.open(ais);
-			clip.start();
-		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-			e.printStackTrace();
+		if(song == null) {
+			for(int i = 0; i < 300; i++) {
+				if(songs[i] != null) {
+					trackNum = i;
+				}
+			}
 		}
-		
+		System.out.println("Playing File: " + song.getAbsoluteFile());
+		if(music.isPlaying()) {
+			music.stop();
+		}
+		music.loadFile(song.getAbsolutePath());
+		music.play();
+			
 	}
-	
 }
